7ca9d94241be11dcab236ca7eaa53e4d4cd89ceb
Gavin Schriver <alexanderschriver@gmail.com>
2020-12-20 20:14:00 -0600

    snapshot is a thing

--- whereithurtsapi/views/Profile.py
+++ whereithurtsapi/views/Profile.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
-from whereithurtsapi.models import Patient, Healing, Treatment
+from whereithurtsapi.models import Patient, Healing, Treatment, Hurt
 from rest_framework import status
 from django.utils import timezone
 from datetime import timedelta
@@ -11,12 +11,18 @@ from datetime import timedelta
 class ProfileHealingSerializer(ModelSerializer):
     class Meta: 
         model = Healing
-        fields = ('id', 'duration', 'date_added')
+        fields = ('id', 'duration', 'date_added', 'notes')
 
 class ProfileTreatmentSerializer(ModelSerializer):
     class Meta:
         model = Treatment
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'notes', 'bodypart')
+        depth = 1
+
+class ProfileHurtSerializer(ModelSerializer):
+    class Meta:
+        model = Hurt
+        fields = ('id', 'name', 'date_added', 'pain_level')
 
 
 class ProfileViewSet(ViewSet):
@@ -36,15 +42,19 @@ class ProfileViewSet(ViewSet):
 
         snapshot = {}
 
+        #retrieve qset of this patient's healings for the past week
         recent_healings =  patient.healing_set.filter(added_on__gte=timezone.now() - timedelta(days=7))
 
-        recent_healing_time = recent_healings.aggregate(Sum('duration'))
 
+        recent_healing_duration = recent_healings.aggregate(Sum('duration'))
+        formatted_healing_time = timedelta(seconds=recent_healing_duration["duration__sum"])
         recent_treatments = Treatment.objects.filter(healing_treatments__healing__in=recent_healings)
+        recent_hurts = Hurt.objects.filter(hurt_healings__healing__in=recent_healings)
 
         snapshot["recent_healings"] = ProfileHealingSerializer(recent_healings, many=True, context={'requet': request}).data
         snapshot["recent_treatments"] = ProfileTreatmentSerializer(recent_treatments, many=True, context={'request': request}).data
-        snapshot["recent_healing_time"] = recent_healing_time["duration__sum"]
+        snapshot["recent_hurts"] = ProfileHurtSerializer(recent_hurts, many=True, context={'request': request}).data
+        snapshot["recent_healing_time"] = str(formatted_healing_time)
 
 
         return Response(snapshot)
