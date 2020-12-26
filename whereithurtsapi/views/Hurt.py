cef4bed8b5dd0fdbbb691ff09c9538a5b1d88a4c
Gavin Schriver <alexanderschriver@gmail.com>
2020-12-25 22:01:05 -0600

    adding recently updated sort by option to hurts

--- whereithurtsapi/views/Hurt.py
+++ whereithurtsapi/views/Hurt.py
@@ -8,42 +8,54 @@ from whereithurtsapi.models import Hurt, Patient, Update, HurtTreatment, Treatme
 from django.utils import timezone
 from itertools import chain
 from operator import attrgetter
+from django.db.models import Max
 
-#Serializers
+# Serializers
+
 
 class UpdateSerializer(ModelSerializer):
     class Meta:
         model = Update
-        fields = ('id', 'added_on', 'notes', 'pain_level', 'is_first_update', 'date_added')
+        fields = ('id', 'added_on', 'notes', 'pain_level',
+                  'is_first_update', 'date_added')
+
 
 class TreatmentLinkSerializer(ModelSerializer):
     class Meta:
         model = TreatmentLink
         fields = ('id', 'linktext', 'linkurl')
 
+
 class HealingSerializer(ModelSerializer):
     class Meta:
         model = Healing
         fields = ('id', 'date_added', 'added_on')
+
 
 class TreatmentSerializer(ModelSerializer):
     links = TreatmentLinkSerializer(many=True)
     """ JSON serializer for Treatments to embed on Hurts """
     class Meta:
         model = Treatment
-        fields = ('id', 'name', 'added_on', 'added_by', 'notes', 'public', 'links')
+        fields = ('id', 'name', 'added_on', 'added_by',
+                  'notes', 'public', 'links')
+
 
 class HurtSerializer(ModelSerializer):
     """JSON serializer for the Hurt model"""
     healings = HealingSerializer(many=True)
     treatments = TreatmentSerializer(many=True)
     updates = UpdateSerializer(many=True)
+
     class Meta:
         model = Hurt
-        fields = ('id','patient', 'healings', 'date_added', 'bodypart', 'name', 'added_on', 'is_active', 'notes', 'pain_level', 'healing_count', 'treatments', 'updates', 'last_update', 'first_update_id', 'owner', 'latest_pain_level')
+        fields = ('id', 'patient', 'healings', 'date_added', 'bodypart', 'name', 'added_on', 'is_active', 'notes', 'pain_level',
+                  'healing_count', 'treatments', 'updates', 'last_update', 'first_update_id', 'owner', 'latest_pain_level')
         depth = 1
 
-#Viewset 
+# Viewset
+
+
 class HurtViewSet(ViewSet):
     def retrieve(self, request, pk=None):
         """ Access a single Hurt """
@@ -51,22 +63,22 @@ class HurtViewSet(ViewSet):
             hurt = Hurt.objects.get(pk=pk)
         except Hurt.DoesNotExist:
             return Response({'message': 'hurt does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         hurt.owner = False
 
         if hurt.patient == Patient.objects.get(user=request.auth.user):
-            hurt.owner = True    
+            hurt.owner = True
 
         serializer = HurtSerializer(hurt, context={'request': request})
         hurt_data = serializer.data
 
-        #serialize matching Healings for this Hurt into list of dicts, then add type
+        # serialize matching Healings for this Hurt into list of dicts, then add type
 
         healings = Healing.objects.filter(hurt_healings__hurt=hurt)
         healings_list = HealingSerializer(healings, many=True).data
         for healing in healings_list:
-            healing.update({"history_type":"Healing"})
-        
+            healing.update({"history_type": "Healing"})
+
         # serialize matching updates. Add a type, and also a Created if it was the first update
         updates = hurt.update_set.all()
         updates_list = UpdateSerializer(updates, many=True).data
@@ -75,8 +87,8 @@ class HurtViewSet(ViewSet):
                 update.update({"history_type": "Created on"})
             else:
                 update.update({"history_type": "Update"})
-                
-        #combine the two lists into one called history
+
+        # combine the two lists into one called history
         history = healings_list + updates_list
 
         # history is returned with newest first by default; check for q string to see if this is flipped with "oldest"
@@ -88,7 +100,7 @@ class HurtViewSet(ViewSet):
                 reverse = False
 
         # sorting function to compare by datetime value of "added_on"
-        by_date = lambda row : row["added_on"]
+        def by_date(row): return row["added_on"]
 
         history = sorted(history, key=by_date, reverse=reverse)
 
@@ -97,26 +109,48 @@ class HurtViewSet(ViewSet):
 
         return Response(hurt_data, status=status.HTTP_200_OK)
 
-
     def list(self, request):
         """Access a list of some/all Hurts"""
 
         hurts = Hurt.objects.all()
 
-        #e.g. /hurts?patient_id=1
+        # e.g. /hurts?patient_id=1
         patient_id = self.request.query_params.get('patient_id', None)
         if patient_id is not None:
             hurts = hurts.filter(patient_id=patient_id)
+
+        show_inactive = self.request.query_params.get('show_inactive', None)
+        if show_inactive is not None:
+            if show_inactive == "false":
+                hurts = hurts.filter(is_active=True)
+
+        bodypart_id = self.request.query_params.get('bodypart_id', None)
+        if bodypart_id is not None:
+            hurts = hurts.filter(bodypart_id=bodypart_id)
+
+        order_by = self.request.query_params.get('order_by', None)
+        if order_by is not None:
+            # e.g. order_by=added_on-asc ; 'added_on' will be order, 'asc' will be direction
+            order = order_by.split('-')[0]
+            direction = order_by.split('-')[1]
+            if order == 'recently_updated':
+                hurts = hurts.annotate(
+                    most_recent_update=Max('update__added_on'))
+                order = 'most_recent_update'
+            if direction == "desc":
+                hurts = hurts.order_by(f"-{order}")
+            if direction == "asc":
+                hurts = hurts.order_by(f"{order}")
 
         for hurt in hurts:
             hurt.owner = False
 
             if hurt.patient == Patient.objects.get(user=request.auth.user):
                 hurt.owner = True
-        
-        serializer = HurtSerializer(hurts, many=True, context={'request': request})
-        return Response(serializer.data)
 
+        serializer = HurtSerializer(
+            hurts, many=True, context={'request': request})
+        return Response(serializer.data)
 
     def create(self, request):
         """ Handle POST operations to /hurts
@@ -141,7 +175,7 @@ class HurtViewSet(ViewSet):
         try:
             hurt.save()
         except ValidationError as ex:
-            return Response({"reason": ex.message })
+            return Response({"reason": ex.message})
 
         # extract treatment ids from request and try to convert that collection to a queryset of Treatment instances
         treatment_ids = request.data["treatment_ids"]
@@ -157,8 +191,8 @@ class HurtViewSet(ViewSet):
             hurt_treatment = HurtTreatment(
                 hurt=hurt, treatment=treatment)
             hurt_treatment.save()
-        
-        #create an update for this Hurt
+
+        # create an update for this Hurt
         update = Update()
         update.hurt = hurt
         update.added_on = timezone.now()
@@ -168,16 +202,16 @@ class HurtViewSet(ViewSet):
 
         serializer = HurtSerializer(hurt, context={'request': request})
         return Response(serializer.data)
-    
+
     def update(self, request, pk=None):
         """ Method to update a Hurt. Also calls and updates the assocaited first update 
             'added_on' and 'patient' values are not subject to update
         """
-        try: 
+        try:
             hurt = Hurt.objects.get(pk=pk)
         except Hurt.DoesNotExist:
             return Response({'message': 'Hurt does not exist'}, status.HTTP_404_NOT_FOUND)
-        
+
         # Save basic model values from request body
         hurt.name = request.data["name"]
         hurt.is_active = request.data["is_active"]
@@ -189,8 +223,8 @@ class HurtViewSet(ViewSet):
         try:
             hurt.save()
         except ValidationError as ex:
-            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)       
-        
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
         # extract treatment ids from request and try to convert that collection to a queryset of Treatment instances
         treatment_ids = request.data["treatment_ids"]
 
@@ -204,20 +238,22 @@ class HurtViewSet(ViewSet):
         current_hurt_treatments = HurtTreatment.objects.filter(hurt=hurt)
         current_hurt_treatments.exclude(treatment__in=treatments).delete()
 
-        # for the treatments that were still in (or are now added to) that array of ids, see if the relationship exists already; if not, create it 
+        # for the treatments that were still in (or are now added to) that array of ids, see if the relationship exists already; if not, create it
         for treatment in treatments:
             try:
                 current_hurt_treatments.get(treatment=treatment)
             except HurtTreatment.DoesNotExist:
-                new_hurt_treatment = HurtTreatment(hurt=hurt, treatment=treatment)
+                new_hurt_treatment = HurtTreatment(
+                    hurt=hurt, treatment=treatment)
                 new_hurt_treatment.save()
-        
+
         # find and update the first associated Update for this Hurt
         try:
-            first_update = Update.objects.get(id=request.data["first_update_id"])
+            first_update = Update.objects.get(
+                id=request.data["first_update_id"])
         except Update.DoesNotExist:
             return Response({'message': 'update does not exist, which is not so good'}, status=status.HTTP_404_NOT_FOUND)
-        
+
         first_update.notes = request.data["notes"]
         first_update.pain_level = request.data["pain_level"]
         first_update.save()
@@ -225,7 +261,7 @@ class HurtViewSet(ViewSet):
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
-        
+
         req_patient = Patient.objects.get(user=request.auth.user)
 
         try:
@@ -239,5 +275,3 @@ class HurtViewSet(ViewSet):
         hurt.delete()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
-
-
